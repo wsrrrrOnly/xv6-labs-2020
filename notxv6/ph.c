7ac5abbe5ccc0ddf5b67964052dc94bc17971355
wsrrrOnly <3358335277@qq.com>
2025-12-16 10:39:33 +0800

    implement ph.c

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t lock[NBUCKET] = { PTHREAD_MUTEX_INITIALIZER };
+
 struct entry {
   int key;
   int value;
@@ -51,7 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock[i]);          // ← 加锁
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);        // ← 解锁
   }
 }
 
@@ -144,4 +148,7 @@ main(int argc, char *argv[])
 
   printf("%d gets, %.3f seconds, %.0f gets/second\n",
          NKEYS*nthread, t1 - t0, (NKEYS*nthread) / (t1 - t0));
+
+  return 0;
 }
+
